40ae4cca105f978506e333e3cb022177a7115a37
Alex Dekau <alex.dekau@tylertech.com>
2021-08-09 11:08:39 -0400

    lint fixes

--- src/default.spec.ts
+++ src/default.spec.ts
@@ -6,9 +6,9 @@ describe('Data Generators: Default', () => {
     it('should default undefined values', () => {
         const gen = constantSequence(undefined, undefined, 'hello').pipe(withDefault(stringGenerator()));
 
-		expect(gen.create().length).toBe(10);
-		expect(gen.create().length).toBe(10);
-		expect(gen.create().length).toBe(5);
-		expect(gen.create().length).toBe(10);
+        expect(gen.create().length).toBe(10);
+        expect(gen.create().length).toBe(10);
+        expect(gen.create().length).toBe(5);
+        expect(gen.create().length).toBe(10);
     });
 });
--- src/default.ts
+++ src/default.ts
@@ -3,14 +3,14 @@ import { DataGenerator } from './data-generator.interface';
 
 /**
  * Data Generator pipe operator for using a default generator in place of undefined outputs.
- * 
+ *
  * @param defaultGenerator default generator to use when undefined is outputted
  */
 export const withDefault =
     <T>(defaultGenerator: DataGenerator<T>) =>
-	/**
-	 * @param dg Data Generator for which to use a default generator in place of undefined values
-	 * @returns a new generator, replacing undefined with a value from the default generator
-	 */
+    /**
+     * @param dg Data Generator for which to use a default generator in place of undefined values
+     * @returns a new generator, replacing undefined with a value from the default generator
+     */
     (dg: DataGenerator<T | undefined>) =>
         dg.flatMap((out) => (out ? constant(out) : defaultGenerator));
