41bbe76c43c8a2dbc6d3e28aebcfad4caf09cd2f
Alex Dekau <alex.potatofarms@gmail.com>
2023-10-01 16:22:17 -0400

    more config updates

--- packages/compiler/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as compile } from './src/compiler';
-export { IDataGeneratorCompilerConfig } from './src/configure';
-export { CONSTANTS } from './src/constants';
-export { default } from './src/transformer';
---
+++ packages/compiler/src/index.ts
@@ -0,0 +1,4 @@
+export { default as compile } from './compiler';
+export { IDataGeneratorCompilerConfig } from './configure';
+export { CONSTANTS } from './constants';
+export { default } from './transformer';
